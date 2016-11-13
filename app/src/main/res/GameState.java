36fc75630ad4f110b6eeda70f3fde760481eaca1
Ashley Dvorsky <advorsky@ashley-big-mac.student.iastate.edu>
2016-11-13 17:26:32 -0600

    made files

---
+++ app/src/main/res/GameState.java
@@ -0,0 +1,23 @@
+/**
+ * Created by advorsky on 11/13/16.
+ */
+
+public class GameState {
+
+    //currentCard
+    //player[]
+    //stack
+    public Card currentCard()
+    {
+        return null;
+    }
+
+    public Player[] returnPlayerArray()
+    {
+        return null;
+    }
+    public Player returnCurrentPlayer()
+    {
+        return null;
+    }
+}
