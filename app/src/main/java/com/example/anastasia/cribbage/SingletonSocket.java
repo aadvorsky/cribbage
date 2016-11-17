88cedb9c0ce5087d8e2cc65cb607c61269725e38
Susan Maslo <maslose13@gmail.com>
2016-11-17 16:41:49 -0600

    Fixed compiler errors in SingletonSocket.

--- app/src/main/java/com/example/anastasia/cribbage/SingletonSocket.java
+++ app/src/main/java/com/example/anastasia/cribbage/SingletonSocket.java
@@ -1,17 +1,16 @@
 package com.example.anastasia.cribbage;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
  * Singleton socket class with support for writing or reading lines to/from the server.
  */
 public class SingletonSocket {
-  private static final RETRY_COUNT = 2;
+  private static final int RETRY_COUNT = 2;
   private static Scanner scanner;
   private static PrintWriter printWriter;
 
@@ -20,54 +19,42 @@ public class SingletonSocket {
    *
    * @return true if successful, false otherwise.
    **/
-  public static false initialize(String ipAddress, int port) {
-    if (socket != null) {
+  public static boolean initialize(String ipAddress, int port) {
+    if (scanner != null) {
       throw new IllegalStateException("Cannot create new socket after one is already created.");
     }
     try {
       Socket socket = new Socket(ipAddress, port);
-      this.scanner = new Scanner(socket.getInputStream());
-      this.printWriter = new PrintWriter(socket.getOutputStream(), true);
+      scanner = new Scanner(socket.getInputStream());
+      printWriter = new PrintWriter(socket.getOutputStream(), true);
+      return true;
+    } catch (UnknownHostException e) {
     } catch (IOException e) {
-      return false;
     }
-    return true;
+    return false;
   }
 
   /**
    * Reads next line from socket. Blocks until line is read.
    *
-   * @return nex line from socket or null if there was a failure.
+   * @return next line from socket.
    **/
   public static String readline(){
     if (scanner == null) {
       throw new IllegalStateException("SingletonSocket not initialized.");
     }
-    for (int i = 0; i < RETRY_COUNT) {
-      try {
-        return scanner.nextLine();
-      } catch (IOException e) {
-      }
-    }
-    return null;
+    return scanner.nextLine();
   }
 
   /**
    * Writes a line to the socket. Blocks until line is written.
    *
    * @param s String to be written
-   * @return true if successful, false otherwise.
-  public static boolean writeLine(String s) {
+   **/
+  public static void writeLine(String s) {
     if (printWriter == null) {
       throw new IllegalStateException("SingletonSocket not initialized.");
     }
-    for (int i = 0; i < RETRY_COUNT; i++) {
-      try {
-        printWriter.println(s);
-        return true;
-      } catch (IOException e) {
-      }
-    }
-    return false;
+    printWriter.println(s);
   }
 }
